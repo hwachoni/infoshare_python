86a23ac2d9f13737ecc8e908330772a59d4685a2
hwachoni <helena.wachonin@intel.com>
2025-10-27 18:14:54 +0100

    Update day1_homework8.py

--- day1/day1_homework8.py
+++ day1/day1_homework8.py
@@ -2,9 +2,11 @@
 #         #
 #       ###
 #     #####
+import math
 pyramid_height_input = input('Podaj wysokość piramidy: ')
 pyramid_height = int(pyramid_height_input)
+rounded = math.ceil(pyramid_height / 2)
 count = -1
-while count < pyramid_height + 2:
+while count <= (pyramid_height + rounded):
     print(f'{"#" * (count + 2):^{(pyramid_height * 2)}}')
     count += 2
